13b76f2797f2618101e974c33446826b4ff7571b
wuyupei <495174699@qq.com>
2023-08-23 13:46:59 +0800

    demo 3 4

---
+++ src/pages/index/04.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="full">
+    <iframe
+      class="full"
+      src="http://3dclock.wuyupei.top"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
