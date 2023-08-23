d7ba8b864b2264a9fe3fae9c00a8b34aab19f555
wuyupei <495174699@qq.com>
2023-08-23 14:28:06 +0800

    threejs screen

--- src/pages/index/05.vue
+++ src/pages/index/05.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="">55</div>
+  <div class="music-three full"></div>
 </template>
 
 <script setup lang="ts">
-import { useTitle } from '@vueuse/core';
+import { onMounted } from 'vue';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-useTitle('05');
+onMounted(() => {
+  const containerEl = document.querySelector('.music-three')!;
+
+  // 场景
+  const scene = new THREE.Scene();
+
+  // 相机
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    containerEl.clientWidth / containerEl.clientHeight,
+    0.1,
+    3000
+  );
+  camera.position.set(0, 0, 500);
+
+  // 渲染器
+  const renderer = new THREE.WebGLRenderer({});
+  renderer.setSize(containerEl.clientWidth, containerEl.clientHeight);
+  document.querySelector('.music-three')!.appendChild(renderer.domElement);
+
+  // 创建一个控制器
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+
+  // 坐标系
+  const axesHelper = new THREE.AxesHelper(containerEl.clientWidth / 5);
+  scene.add(axesHelper);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
+  window.addEventListener('resize', () => {
+    camera.aspect = containerEl.clientWidth / containerEl.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(containerEl.clientWidth, containerEl.clientHeight);
+  });
+});
 </script>
 
-<style scoped></style>
+<style scoped lang="less"></style>
